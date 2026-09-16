3280592ca45e6a2c1cb8d97f446a334e8bb99a57
Mohsin Ali <=>
2026-09-16 09:08:40 +0500

    Updated project files

--- config/db.js
+++ config/db.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 let connectionPromise;
-
 export const connectDB = async () => {
     if (mongoose.connection.readyState === 1) {
         return mongoose.connection;
